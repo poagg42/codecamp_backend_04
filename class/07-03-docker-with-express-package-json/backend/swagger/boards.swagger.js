12cf068d17894a8ef258acf100ab2b86fcc4bcda
poagg42 <poagg42@gmail.com>
2022-07-12 19:01:29 +0900

    -m

---
+++ class/07-03-docker-with-express-package-json/backend/swagger/boards.swagger.js
@@ -0,0 +1,43 @@
+/**
+ * @swagger
+ * /boards:
+ *   get:
+ *     summary: 게시글 가져오기
+ *     tags: [Board]
+ *     parameters:
+ *          - in: query
+ *            name: number
+ *            type: int
+ *     responses:
+ *       200:
+ *         description: 성공
+ *         content:
+ *           application/json: 
+ *              schema:
+ *                  type: array
+ *                  items: 
+ *                      properties:
+ *                          number:
+ *                              type: int 
+ *                              example: 1
+ *                          writer:
+ *                              type: string
+ *                              example: 철수
+ *                          title:
+ *                              type: string
+ *                              example: 좋은 아침입니다~
+ *                          contents:
+ *                              type: string
+ *                              example: 오늘 하루도 화이팅하세요!
+ */
+
+/**
+ * @swagger
+ * /boards:
+ *   post:
+ *     summary: 게시글 등록하기
+ *     tags: [Board]
+ *     responses:
+ *       200:
+ *         description: 성공
+ * */
