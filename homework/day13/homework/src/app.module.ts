d33f236cc605c2c70fbd4f43359aa350e6cb5fc0
poagg42 <poagg42@gmail.com>
2022-07-21 21:20:20 +0900

    7월 21일

---
+++ homework/day13/homework/src/app.module.ts
@@ -0,0 +1,20 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { BoardModule } from './board/board.module';
+
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    BoardModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'src/commons/graphql/schema.gql',
+    }),
+  ],
+  // controllers: [AppController],
+  // providers: [AppService],
+})
+export class AppModule {}
